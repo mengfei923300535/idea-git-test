c286cb6168b324c0d483ace9c48218f9c9c3c534
Your Name <you@example.com>
2020-06-03 15:31:06 +0900

    项目初始化

---
+++ idea-git-test/src/test01.java
@@ -0,0 +1,11 @@
+﻿/**
+ * @author Fei
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/6/3 15:22
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
